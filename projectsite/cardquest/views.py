c68409f630b314464df8a66b26df0454522bfe50
s0wee <mart.edrian.bernardo3.com>
2023-12-09 00:47:01 +0800

    static folder and views.py

--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from cardquest.models import PokemonCard
 
 # Create your views here.
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = "home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
