04de16259e97de35d58a0706b717a48532d63d4c
s0wee <mart.edrian.bernardo3.com>
2023-12-09 01:42:01 +0800

    done static

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import PokemonCard
+from .models import PokemonCard, Trainer
 
 # Register your models here.
 #admin.site.register(PokemonCard)
 @admin.register(PokemonCard)
-
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ("name", "rarity")
+    search_fields = ("name",)
+    
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ("name", "birthdate", "location", "email")
     search_fields = ("name",)
--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from cardquest.models import PokemonCard
+from cardquest.models import PokemonCard, Trainer
 
 # Create your views here.
 class HomePageView(ListView):
@@ -11,3 +11,9 @@ class HomePageView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+    
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 15
